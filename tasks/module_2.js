6fdc6926aa11f99dae0045da4e6ea7454881d680
shterneregen <shterneregen@gmail.com>
2020-02-17 19:26:52 +0300

    module 2 & materials

---
+++ tasks/module_2.js
@@ -0,0 +1,68 @@
+console.log("==============================================================");
+console.log("==== Module#2 Functions ====");
+console.log("==============================================================");
+console.log("==== Currying ====");
+console.log("==============================================================");
+const mergeWords = (w1) => {
+    return (w2) => {
+        return (w3) => {
+            return (w4) => {
+                return (w5) => {
+                    return [w1, w2, w3, w4].join(" ");
+                };
+            };
+        };
+    };
+};
+
+const result = mergeWords("GNU")("is")("not")("Unix.")();
+console.log(result);
+
+console.log("==============================================================");
+console.log("==== Every/Some ====");
+console.log("==============================================================");
+const goodUsers = [{ id: 1 }, { id: 2 }, { id: 3 }];
+// `checkUsersValid` is the function you'll define
+const checkUsersValid = (goodArr) => {
+    return function(arrToCheck) {
+        let fine = true;
+        for (let e1 of arrToCheck) {
+            let count = 0;
+            for (let e2 of goodArr) {
+                if (e1["id"] === e2["id"]) {
+                    count++;
+                    break;
+                }
+            }
+            fine = fine && count > 0;
+            if (!fine) break;
+        }
+        return fine;
+    };
+};
+
+const testAllValid = checkUsersValid(goodUsers);
+
+const result1 = testAllValid([{ id: 2 }, { id: 1 }]);
+console.log("Only valid", result1);
+
+const result2 = testAllValid([{ id: 2 }, { id: 4 }, { id: 1 }]);
+console.log("Only valid", result2);
+
+console.log("==============================================================");
+console.log("==== Reduce ====");
+console.log("==============================================================");
+const countWords = (inputWords) => {
+    let map = new Map();
+    for (let f of inputWords) {
+        let i = typeof map.get(f) == "undefined" ? 1 : map.get(f) + 1;
+        map.set(f, i);
+    }
+    return Array.from(map).reduce(
+        (obj, [key, value]) => Object.assign(obj, { [key]: value }),
+        {}
+    );
+};
+
+const inputWords = ["Apple", "Banana", "Apple", "Durian", "Durian", "Durian"];
+console.log(countWords(inputWords));
