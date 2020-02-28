d4ab9467e9557984176ec9d4955bf401d979114e
shterneregen <shterneregen@gmail.com>
2020-02-28 10:51:02 +0300

    module 2 upgrade

--- tasks/module_2.js
+++ tasks/module_2.js
@@ -3,17 +3,13 @@ console.log("==== Module#2 Functions ====");
 console.log("==============================================================");
 console.log("==== Currying ====");
 console.log("==============================================================");
-const mergeWords = (w1) => {
-    return (w2) => {
-        return (w3) => {
-            return (w4) => {
-                return (w5) => {
-                    return [w1, w2, w3, w4].join(" ");
-                };
-            };
-        };
-    };
-};
+const mergeWords = (param) => {
+  return (nextParam) => {
+    return nextParam
+        ? mergeWords(`${param} ${nextParam}`)
+        : param;
+  }
+}
 
 const result = mergeWords("GNU")("is")("not")("Unix.")();
 console.log(result);
@@ -48,10 +44,7 @@ const countWords = (inputWords) => {
         let i = typeof map.get(f) == "undefined" ? 1 : map.get(f) + 1;
         map.set(f, i);
     }
-    return Array.from(map).reduce(
-        (obj, [key, value]) => Object.assign(obj, { [key]: value }),
-        {}
-    );
+    return Array.from(map).reduce((obj, [key, value]) => Object.assign(obj, {[key]: value }), {});
 };
 
 const inputWords = ["Apple", "Banana", "Apple", "Durian", "Durian", "Durian"];
@@ -61,13 +54,9 @@ console.log("==============================================================");
 console.log("==== Palindrome ====");
 console.log("==============================================================");
 const isPalindrome = (string) => {
-    let pal =
-        string ===
-        string
-            .split("")
-            .reverse()
-            .join("");
-    return pal ? "The entry is a palindrome" : "Entry is not a palindrome";
+    return string ===  string.split("").reverse().join("")
+        ? "The entry is a palindrome"
+        : "Entry is not a palindrome";
 };
 
 console.log(isPalindrome("madam"));
@@ -87,8 +76,7 @@ console.log(factorial(5));
 console.log("==============================================================");
 const amountToCoins = (expectedSum, coins, ...args) => {
     let resultCoins = typeof args[0] === "undefined" ? [] : args[0];
-    let currentSum =
-        resultCoins.length === 0 ? 0 : resultCoins.reduce((a, b) => a + b);
+    let currentSum = resultCoins.length === 0 ? 0 : resultCoins.reduce((a, b) => a + b);
     let currentCoin = coins[0];
     if (currentSum + currentCoin === expectedSum) {
         resultCoins.push(currentCoin);
