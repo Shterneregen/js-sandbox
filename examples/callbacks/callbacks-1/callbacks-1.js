e25a961acea38763e982f5a6c1135c0bee75c48e
shterneregen <shterneregen@gmail.com>
2020-03-13 16:15:27 +0300

    callback & promise example

---
+++ examples/callbacks/callbacks-1/callbacks-1.js
@@ -0,0 +1,27 @@
+// Async JS Crash Course - Callbacks, Promises, Async Await
+// https://www.youtube.com/watch?v=PoRJizFvM7s
+
+const posts = [
+    { title: "Post One", body: "This is post one" },
+    { title: "Post Two", body: "This is post two" }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = "";
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+function createPost(post, callback) {
+    setTimeout(() => {
+        posts.push(post);
+        callback();
+    }, 2000);
+}
+
+// getPosts();
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
