b35275cb66cc14d60a70531232e4db331bb96404
shterneregen <shterneregen@gmail.com>
2020-03-29 23:58:00 +0300

    css before examples

---
+++ css-basics/before-after/before-1/before.js
@@ -0,0 +1,6 @@
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('done');
+    }
+}, false);
