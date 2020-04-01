7a63e4f2ae87250ce81777314b1cff43d313665f
shterneregen <shterneregen@gmail.com>
2020-04-01 21:28:49 +0300

    module 7, task 3

--- tasks/module_7/3/slider.js
+++ tasks/module_7/3/slider.js
@@ -1,24 +1,32 @@
-let slidesUrls = [];
 let imageCounter = 0;
+let autoSlide;
+let slideIndex = 1;
+const defaultTimerIntervalSec = 2;
+
+let timer = document.getElementById("timer");
+timer.addEventListener("change", () => startAutoSliding());
+timer.value = defaultTimerIntervalSec;
+
 let imageUrl = document.getElementById("imageUrl");
 let addButton = document.getElementById("addButton");
-addButton.addEventListener('click', () => addSlide(imageUrl.value));
+addButton.addEventListener("click", () => addSlide(imageUrl.value));
 
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
+function plusSlides(n, withoutStopping) {
     showSlides(slideIndex += n);
+    if (!withoutStopping) {
+        stopAutoSliding();
+    }
 }
 
 function showSlides(n) {
     let slides = document.getElementsByClassName("blogSlides");
     if (!slides || slides.length === 0) return;
+
     let i;
     if (n > slides.length) {
         slideIndex = 1
-    }
-    if (n < 1) {
+    } else if (n < 1) {
         slideIndex = slides.length
     }
     for (i = 0; i < slides.length; i++) {
@@ -28,20 +36,65 @@ function showSlides(n) {
 }
 
 function addSlide(url) {
-    console.log("Adding slide", url);
-    imageCounter++;
-    // slidesUrls.push(url);
+    loadImage(url).then((image) => {
+        imageCounter++;
+        let slidesContainer = document.getElementById("slider-container");
+        slidesContainer.appendChild(createSlide(image));
+        showSlides(slideIndex);
+        imageUrl.value = "";
+        if (imageCounter > 1) {
+            startAutoSliding();
+        }
+    }).catch((error) => {
+        console.log("Some error:", error);
+    });
+}
+
+let createSlide = (newImg) => {
     const newSlide = document.createElement("div");
     newSlide.className = "blogSlides fade";
     newSlide.id = "slide_" + imageCounter;
-    newSlide.addEventListener("dblclick", () => newSlide.remove());
-
-    const newImg = document.createElement("img");
-    newImg.src = url;
+    newSlide.addEventListener("dblclick", () => removeSlide(newSlide));
     newSlide.appendChild(newImg);
+    return newSlide;
+};
 
-    let slidesContainer = document.getElementById("slider-container");
-    slidesContainer.appendChild(newSlide);
+let removeSlide = (slide) => {
+    if (confirm("Are you sure you want to remove slide?")) {
+        plusSlides(1);
+        slide.remove();
+        imageCounter--;
+    }
+};
 
-    showSlides(slideIndex);
-}
+let startAutoSliding = () => {
+    if (imageCounter < 2) {
+        return;
+    }
+    console.log("Start auto sliding, interval =", timer.value);
+    clearInterval(autoSlide);
+    autoSlide = setInterval(() => plusSlides(1, true),
+        timer.value * 1000 || defaultTimerIntervalSec * 1000)
+};
+
+let stopAutoSliding = () => {
+    console.log("Stop auto sliding");
+    clearInterval(autoSlide)
+};
+
+let loadImage = (url) => {
+    return new Promise((resolve, reject) => {
+        let image = new Image();
+
+        image.onload = function () {
+            resolve(image);
+        };
+
+        image.onerror = function () {
+            let message = "Could not load image at " + url;
+            reject(new Error(message));
+        };
+
+        image.src = url;
+    });
+};
